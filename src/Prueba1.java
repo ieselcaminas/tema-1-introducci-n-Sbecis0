5fed64c1093a0c27841122950721abc460c9daba
Sbecis0 <abdbecsal@ealu.edu.gav.es>
2024-09-20 11:42:06 +0200

    Prueba1

---
+++ src/Prueba1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Prueba{
+    public static void main(String[] args) {
+
+        int num;
+        int positive = 0;
+
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 10; i++) {
+            num = sc.nextInt();
+            if (num > 0) {
+                positive ++;
+
+
+            }
+        }
+        System.out.println("Positivos " +positive);
+    }
+}
