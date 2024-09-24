fa2011c0f7d3c852fcd053475bbb4538492e6b6b
Sbecis0 <abdbecsal@ealu.edu.gav.es>
2024-09-24 12:42:43 +0200

    Factorial25

---
+++ src/Factorial25.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Factorial25 {
+    public static void main(String[] args) {
+
+        long factorial = 1;
+        int num1;
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        num1 = sc.nextInt();
+
+        for (int i = 1;
+             i <= num1;
+             i++) {
+            factorial = factorial * i;
+        }
+            System.out.println("El factorial de " +num1 + " es " + factorial);
+
+
+
+
+    }
+
+
+}
