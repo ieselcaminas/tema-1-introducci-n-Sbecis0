bef2f859d5fd79e53c297c25ac3c7d872a2a00f1
Sbecis0 <abdbecsal@ealu.edu.gav.es>
2024-09-24 13:41:01 +0200

    Impresion27

---
+++ src/Divisores31.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Divisores31 {
+    public static void main(String[] args) {
+
+        int num1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        num1 = sc.nextInt();
+
+        for (int i = 1; i <= num1; i++) {
+            if (num1 % i == 0) {
+                System.out.println(+num1+ " és divisor de " +i);
+            }
+        }
+
+
+    }
+}
