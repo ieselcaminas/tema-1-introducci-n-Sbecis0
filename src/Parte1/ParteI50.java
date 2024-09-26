7cdb5b435aed18f8bd784b9accbab2110e946b7e
Sbecis0 <abdbecsal@ealu.edu.gav.es>
2024-09-26 13:07:05 +0200

    50

---
+++ src/Parte1/ParteI50.java
@@ -0,0 +1,17 @@
+package Parte1;
+
+public class ParteI50 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 || i % 3 == 0) {
+                System.out.println(+i);
+
+            }
+        }
+    }
+}
+
+
+
+
