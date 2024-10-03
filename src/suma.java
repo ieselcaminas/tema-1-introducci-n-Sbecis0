3ec4da75c63bcd7d552d21e92a54697e63eca8b7
suma <abdbecsal@alu.edu.gva.es>
2024-10-03 12:04:47 +0200

    suma

---
+++ src/suma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class suma {
+    public static void main(String[] args) {
+        int n;
+        int n2;
+        Scanner sc = new Scanner(System.in);
+        System
+                .out.print("Enter an integer: ");
+        int a = sc.nextInt();
+        sc.close();
+    }
+}
