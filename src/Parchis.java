0010faae97fb10aa0f27a7d46e78307ab2a1511a
suma <abdbecsal@alu.edu.gva.es>
2024-11-14 13:54:49 +0100

    Parchís

---
+++ src/Parchis.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Parchis {
+    public static void main(String[] args) {
+
+        int c;
+        int p;
+        int t;
+        int x;
+
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.print("Introduce un numero de casillas: ");
+            c = sc.nextInt();
+            System.out.print("Introduce una casilla en la que estas: ");
+            p = sc.nextInt();
+            System.out.print("Introduce un numero de dado: ");
+            t = sc.nextInt();
+
+            if (c == 0 && p == 0 && t == 0) {
+                break;
+            }
+
+            int f = p + t;
+
+            if (f > c) {
+                x = f - c;
+                System.out.println(c - x);
+            }else{
+                x = p + t;
+                System.out.println(x);
+            }
+        }while(true);
+    }
+}
